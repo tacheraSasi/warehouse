fadedaf3e8be22592ef81d9f4ddbb08ad21111d2
Tachera Sasi <tacherasasi@gmail.com>
2025-01-14 18:42:14 +0300

    Fetched products from the db

--- src/features/products/components/product-listing.tsx
+++ src/features/products/components/product-listing.tsx
@@ -3,11 +3,11 @@ import { fakeProducts } from '@/constants/mock-api';
 import { searchParamsCache } from '@/lib/searchparams';
 import { DataTable as ProductTable } from '@/components/ui/table/data-table';
 import { columns } from './product-tables/columns';
+import prisma from 'prisma/client';
 
 type ProductListingPage = {};
 
 export default async function ProductListingPage({}: ProductListingPage) {
-  // Showcasing the use of search params cache in nested RSCs
   const page = searchParamsCache.get('page');
   const search = searchParamsCache.get('q');
   const pageLimit = searchParamsCache.get('limit');
@@ -20,9 +20,13 @@ export default async function ProductListingPage({}: ProductListingPage) {
     ...(categories && { categories: categories })
   };
 
-  const data = await fakeProducts.getProducts(filters);
-  const totalProducts = data.total_products;
-  const products: Product[] = data.products;
+  // const data = await fakeProducts.getProducts(filters);
+  // const totalProducts = data.total_products;
+  // const products: Product[] = data.products;
+  
+  const dataFromDb = await prisma.products.findMany()
+  const products = dataFromDb
+  const totalProducts = await prisma.products.count()
 
   return (
     <ProductTable
