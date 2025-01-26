85248c86d0dea5ddf2fb40d40201e7976eade23a
Tachera Sasi <tacherasasi@gmail.com>
2025-01-26 14:03:59 +0300

    more changes to the code base

--- src/app/dashboard/shipments/page.tsx
+++ src/app/dashboard/shipments/page.tsx
@@ -32,11 +32,11 @@ export default async function Page({ searchParams }: pageProps) {
       <div className='space-y-4'>
         <div className='flex items-start justify-between'>
           <Heading   
-            title='Products'
-            description='Manage products (Server side table functionalities.)'
+            title='Shipments'
+            description='Manage Shipments'
           />
           <Link
-            href='/dashboard/product/new'
+            href='/dashboard/shipment/new'
             className={cn(buttonVariants(), 'text-xs md:text-sm')}
           >
             <Plus className='mr-2 h-4 w-4' /> Add New
