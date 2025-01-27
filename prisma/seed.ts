659c8e2c845d74f68366e1878382aba847a2856f
Tachera Sasi <tacherasasi@gmail.com>
2025-01-28 00:33:16 +0300

    seed data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,34 @@
-import { hashPassword } from "@/lib/utils";
-import prisma from "./client";
-import { faker } from "@faker-js/faker";
+import { hashPassword } from '@/lib/utils';
+import prisma from './client';
+import { faker } from '@faker-js/faker';
 
 export default async function main() {
-  
-  const password = await hashPassword("password");
+  const password = await hashPassword('password');
 
   // Seed admin
   await prisma.admins.create({
     data: {
-      name: "Ekilie",
-      email: "support@ekilie.com",
-      password: password,
-    },
+      name: 'Ekilie',
+      email: 'support@ekilie.com',
+      password: password
+    }
+  });
+  await prisma.admins.create({
+    data: {
+      name: 'Tachera Sasi',
+      email: 'tacherasasi@gmail.com',
+      password: password
+    }
+  });
+
+  const categories = Array.from({ length: 20 }, (_, index) => ({
+    name: faker.commerce.department(),
+    createdAt: faker.date.between({ from: '2022-01-01', to: '2025-12-31' }),
+    updatedAt: faker.date.recent()
+  }));
+
+  await prisma.categories.createMany({
+    data: categories
   });
 
   // Seed products
@@ -21,24 +37,28 @@ export default async function main() {
     description: faker.commerce.productDescription(),
     category: faker.commerce.department(),
     price: parseFloat(faker.commerce.price({ min: 5, max: 500 })),
-    photoUrl: `https://api.slingacademy.com/public/sample-products/${index + 1}.png`,
-    createdAt: faker.date.between({from:"2022-01-01", to:"2025-12-31"}),
-    updatedAt: faker.date.recent(),
+    photoUrl: `https://api.slingacademy.com/public/sample-products/${
+      index + 1
+    }.png`,
+    createdAt: faker.date.between({ from: '2022-01-01', to: '2025-12-31' }),
+    updatedAt: faker.date.recent()
   }));
 
   await prisma.products.createMany({
-    data: products,
+    data: products
   });
+
+  
 
   // Seed inventory
   const inventoryData = products.map((_, index) => ({
     productId: index + 1,
     quantity: faker.number.int({ min: 10, max: 100 }),
-    location: faker.location.city(),
+    location: faker.location.city()
   }));
 
   await prisma.inventory.createMany({
-    data: inventoryData,
+    data: inventoryData
   });
 
   // Seed customers
@@ -46,14 +66,14 @@ export default async function main() {
     name: faker.person.fullName(),
     email: faker.internet.email(),
     phone: faker.phone.number(),
-    address: faker.location.streetAddress(),
+    address: faker.location.streetAddress()
   }));
 
   await prisma.customers.createMany({
-    data: customers,
+    data: customers
   });
 
-  console.log("Seeding completed!");
+  console.log('Seeding completed!');
 }
 
 main()
